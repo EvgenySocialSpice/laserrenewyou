8c3d7805d7c0fee4e378e674997c2a09bf440c9d
Evgeny Surogin <sacccp@gmail.com>
2021-08-16 15:10:41 +0300

    Second one

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,10 +1,11 @@
-from flask import Flask
 from flask import Flask, render_template, url_for
+from webapp.model import db
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
+    db.init_app(app)
 
     @app.route('/')
     def index():
