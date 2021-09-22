f919998b57d74ffe59ad343445f232fec7de4f93
Evgeny Surogin <sacccp@gmail.com>
2021-09-22 22:41:57 +0300

    Added login etc

---
+++ create_admin.py
@@ -0,0 +1,23 @@
+from getpass import getpass
+import sys
+from webapp import create_app
+from webapp.model import User, db
+
+
+app = create_app()
+
+with app.app_context():
+    username = input('your name: ')
+    if User.query.filter(User.username == username).count():
+        print('This name is already exist')
+        sys.exit(0)
+    password = getpass('your password: ')
+    password2 = getpass('repeat your password: ')
+    if not password == password2:
+        print('password is not the same')
+        sys.exit(0)
+    new_user = User(username=username, role='admin')
+    new_user.set_password(password)
+    db.session.add(new_user)
+    db.session.commit()
+    print('User with id {} added'.format(new_user.id))
--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, url_for
-from webapp.model import db
+from flask import Flask, render_template, flash, redirect, url_for
+from webapp.forms import LoginForm
+from webapp.model import db, User
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 
 
 def create_app():
@@ -7,9 +9,51 @@ def create_app():
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+
     @app.route('/')
     def index():
         title = 'Beauty laser treatments and calorie burning bed sessions - to make you feel renewed'
         return render_template('index.html', page_title=title)
+
+    @app.route('/login')
+    def login():
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+        title = 'Login'
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
+
+    @app.route('/process-login', methods=['POST'])
+    def process_login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter(User.username == form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash('you are successfully login')
+                return redirect(url_for('index'))
+            flash('wrong password or login name')
+            return redirect(url_for('login'))
+
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash('you are successfully logout')
+        return redirect(url_for('index'))
+
+    @app.route('/admin')
+    @login_required
+    def admin_index():
+        if current_user.is_admin:
+            return 'Hello Admin!'
+        else:
+            return 'Sorry, you are not Admin!'
 
     return app
